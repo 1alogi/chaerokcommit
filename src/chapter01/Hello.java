3a611ca5a0ba32e7b9712e3c1ac19070ab8b4623
306-25 <306-25@DESKTOP-AMF53P2>
2022-02-10 19:54:39 +0900

    220203

---
+++ src/chapter01/Hello.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
+
+//public class 클래스 선언부 hello 클래스이름
+// public static void 메소드 선언부 main 메소드이름 
+//메소드는 어떤일 을 처리하는 실행문들을 모아 놓은 블럭
